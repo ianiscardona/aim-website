49cb9af6e921808d05b0d68838eb0eb67bf3c8f7
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-06-14 21:49:45 +0800

    tech update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,36 +3,64 @@ import React from "react";
 import Marquee from "react-fast-marquee";
 
 export const Home = () => {
+  const technologies1 = [
+    { text: "Javascript", color: "hover:text-yellow-500" },
+    { text: "Typescript", color: "hover:text-sky-500" },
+    { text: "CSS", color: "hover:text-blue-700" },
+    { text: "HTML", color: "hover:text-orange-500" },
+    { text: "React", color: "hover:text-cyan-500" },
+  ];
+  const technologies2 = [
+    { text: "Vite", color: "hover:text-purple-400" },
+    { text: "React Native", color: "hover:text-cyan-600" },
+    { text: "Framer Motion", color: "hover:text-fuchsia-600" },
+    { text: "TailwindCSS", color: "hover:text-sky-400" },
+  ];
+  const technologies3 = [
+    { text: "Firebase", color: "hover:text-amber-400" },
+    { text: "Axios", color: "hover:text-purple-700" },
+    { text: "MySQL", color: "hover:text-orange-500" },
+    { text: "Unlighthouse", color: "hover:text-green-700" },
+    { text: "Trello", color: "hover:text-blue-500" },
+  ];
+
   return (
-    <div className="relative box-border w-full overflow-hidden bg-slate-200 pt-20">
+    <div className="bg-fu relative box-border w-full overflow-hidden bg-slate-200 pt-20">
       <div className="text-8xl">
-        <Marquee speed={20} className="pb-2" direction="right">
+        <Marquee speed={20} className="overflow-hidden pb-2" direction="right">
           <h1 className="font-intertight">
-            <span className="transition-colors duration-500 hover:text-yellow-500">
-              Javascript.
-            </span>
-            <span className="transition-colors duration-500 hover:text-sky-500">
-              Typescript.
-            </span>
-            <span className="transition-colors duration-500 hover:text-blue-700">
-              CSS.
-            </span>
-            <span className="transition-colors duration-500 hover:text-orange-500">
-              HTML.
-            </span>
-            <span className="transition-colors duration-500 hover:text-cyan-500">
-              React.
-            </span>
+            {technologies1.map((tech, index) => (
+              <span
+                key={index}
+                className={`transition-colors duration-500 ${tech.color}`}
+              >
+                {tech.text}.
+              </span>
+            ))}
           </h1>
         </Marquee>
-        <Marquee speed={20} className="pb-2">
+        <Marquee speed={20} className="overflow-hidden pb-2">
           <h1 className="font-intertight">
-            Vite.React Native.TailwindCSS.Framer Motion.
+            {technologies2.map((tech, index) => (
+              <span
+                key={index}
+                className={`transition-colors duration-500 ${tech.color}`}
+              >
+                {tech.text}.
+              </span>
+            ))}
           </h1>
         </Marquee>
-        <Marquee speed={20} className="pb-2" direction="right">
+        <Marquee speed={20} className="overflow-hidden pb-2" direction="right">
           <h1 className="font-intertight">
-            Axios.Firebase.MySQL.NextJS.Javascript.
+            {technologies3.map((tech, index) => (
+              <span
+                key={index}
+                className={`transition-colors duration-500 ${tech.color}`}
+              >
+                {tech.text}.
+              </span>
+            ))}
           </h1>
         </Marquee>
       </div>
